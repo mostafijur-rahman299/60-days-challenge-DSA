1a8e65ca3678f23d984cd21f46eb8f40a12f8b4d
Mostafijur Rahman <mostafijur.rahman299@gmail.com>
2023-08-30 08:35:33 +0600

    #Day-2 another approch for merge two linked list

--- Day-2/Merge-two-list.py
+++ Day-2/Merge-two-list.py
@@ -40,6 +40,27 @@ class LinkedList:
             seek.next, target = target, seek.next
             seek = seek.next
         return head
+    
+    def mergeTwoLists2(self, list1, list2):
+        cur = dummy = LinkedNode()
+        
+        while list1 and list2:               
+            if list1.val < list2.val:
+                cur.next = list1
+                cur = list1
+                list1 = list1.next
+                
+#                 print("list1===", list1.val)
+#                 print("cur====", cur.val)
+            else:
+                cur.next = list2
+                cur = list2
+                list2 = list2.next
+                
+        if list1 or list2:
+            cur.next = list1 if list1 else list2
+            
+        return dummy.next
                 
         
         

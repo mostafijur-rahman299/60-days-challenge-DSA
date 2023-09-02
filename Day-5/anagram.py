e674461b59332ea10f359520c9a54bbe76c14fa9
Mostafijur Rahman <mostafijur.rahman299@gmail.com>
2023-09-02 08:20:54 +0600

    #Day-5 anagram with optimized way

--- Day-5/anagram.py
+++ Day-5/anagram.py
@@ -7,5 +7,21 @@ anagram("anagram", "nagaram")
 anagram("rat", "car")
 
 
-
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        count = [0] * 26
+        
+        for char in s:
+            asci = ord(char) - ord('a')
+            count[asci] += 1
+                    
+        for char in t:
+            asci = ord(char) - ord('a')
+            count[asci] -= 1
+            
+        for val in count:
+            if val != 0:
+                return False
+            
+        return True
 

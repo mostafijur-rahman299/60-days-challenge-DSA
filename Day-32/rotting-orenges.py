f717657de3450164a46eb32ae26303d6dba0390f
Mostafijur Rahman <mostafijur.rahman299@gmail.com>
2023-10-02 07:10:23 +0600

    Rotting oranges

--- Day-32/rotting-orenges.py
+++ Day-32/rotting-orenges.py
@@ -3,45 +3,31 @@ from typing import List
 
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        rows, cols = len(grid), len(grid[0])
-        self.minute = 0
-        visit = set()
+        time, fresh = 0, 0
+        ROWS, COLS = len(grid), len(grid[0])
+        q = collections.deque()
 
-        def bfs(r, c):
-            q = collections.deque()
-            q.append((r, c))
-            visit.add((r, c))
-            
-            while q:
+        for row in range(ROWS):
+            for col in range(COLS):
+                if grid[row][col] == 1:
+                    fresh += 1
+                if grid[row][col] == 2:
+                    q.append((row, col))
+
+        directions = [(0, 1), (0, -1), (-1, 0), (1, 0)]
+
+        while q and fresh > 0:
+            for q_index in range(len(q)):
                 row, col = q.popleft()
-                
-                directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-                can_elasp = False
-                
                 for dr, dc in directions:
-                    r, c = dr + row, dc + col
-                    
-                    if (r in range(rows) and 
-                        c in range(cols) and 
-                        (r, c) not in visit and 
-                        grid[r][c] == 1):
-                        grid[r][c] = 2
-                        q.append((r, c))
-                        visit.add((r, c))
-                        can_elasp = True
+                    r, c = row + dr, col + dc
+                    if (r < 0 or r == len(grid) or
+                    c < 0 or c == len(grid[0]) or
+                    grid[r][c] != 1):
+                        continue
                         
-                if can_elasp:
-                    self.minute += 1
-                
-        for r in range(rows):
-            for c in range(cols):
-                if (r, c) not in visit and grid[r][c] == 2:
-                    bfs(r, c)
-
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == 1:
-                    return -1
-
-        return self.minute
-    
+                    grid[r][c] = 2
+                    q.append((r, c))
+                    fresh -= 1
+            time += 1
+        return time if fresh == 0 else -1

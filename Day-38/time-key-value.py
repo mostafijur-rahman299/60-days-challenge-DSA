50b494a0869fafa672c3da0a34b781ec48c9e3e6
Mostafijur Rahman <mostafijur.rahman299@gmail.com>
2023-10-05 22:19:36 +0600

    time stamp with optimize way

--- Day-38/time-key-value.py
+++ Day-38/time-key-value.py
@@ -29,8 +29,31 @@ class TimeMap:
         
         return timeseries[idx-1][1]
         
+class TimeMap:
+    
+    def __init__(self):
+        self.store = {}
         
+    def set(self, key, value, timestamp):
+        if key not in self.store:
+            self.store[key] = []
+        self.store[key].append([value, timestamp])
         
+    def get(self, key, timestamp):
+        res = ""
+        values = self.store.get(key, [])
+        
+        # binary search
+        l, r = 0, len(values) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if values[m][1] <= timestamp:
+                res = values[m][0]
+                l = m + 1
+            else:
+                r = m - 1
+        return res
+    
         
 timemap = TimeMap()
 timemap.set("foo", "bar", 1)

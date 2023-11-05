d6aaf849d227104ffeb91a65ab2a4d155dc99c3e
Mostafijur Rahman <mostafijur.rahman299@gmail.com>
2023-11-05 08:53:49 +0600

    Add binary practice prompt

--- Day-15/add-binary.py
+++ Day-15/add-binary.py
@@ -2,6 +2,7 @@ class Solution:
     def addBinary(self, a: str, b: str) -> str:
         return bin(int(a, 2) + int(b, 2))[2:]
     
+# Solution 01
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
         def binary_to_decimal(binary_number):
@@ -27,3 +28,51 @@ class Solution:
 
         return decimal_to_binary(binary_to_decimal(int(a)) + binary_to_decimal(int(b)))
     
+    
+# Solution 02
+res = ''
+
+carry = '0'
+a = '11'
+b = '1'
+
+
+if len(a) < len(b):
+    a = len(b[len(a):]) * '0' + a
+if len(b) < len(a):
+    b = len(a[len(b):]) * '0' + b
+    
+
+def b_sum(num1, num2, carry):
+    print(num1, num2, carry)
+    summ = '0'
+    if num1 == '1' and num2 == '1':
+        summ = '10'
+    elif num1 == '0' or num2 == '0':
+        summ =  str(int(num1) or int(num2))
+    if carry == '1':
+        if summ == '10':
+            return '11'
+        elif summ == '1':
+            return '10'
+        else:
+            return carry
+    return summ
+
+for i in range(len(b)-1, -1, -1):
+    summ = b_sum(a[i], b[i], carry)
+    
+    print("sum===", summ)
+    
+    if summ == '1':
+        res = summ + res
+        carry = '0'
+        continue
+        
+    if i == 0:
+        res = summ + res
+        break
+        
+    res = str(int(summ) % 10) + res
+    
+    carry = str(int(summ) // 10) 
